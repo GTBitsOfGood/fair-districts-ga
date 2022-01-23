e741c44706db01ab68cab7c8ce8e37bd2bb848d5
Manu Gupta <mgupta@Manus-MacBook-Pro-7.local>
2022-01-22 21:31:28 -0500

    Setup

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
+  ],
+
+  adapter: PrismaAdapter(prisma),
+})
---
+++ prisma/migrations/20220122222050_auth/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Account" ALTER COLUMN "providerAccountId" SET DATA TYPE TEXT;
