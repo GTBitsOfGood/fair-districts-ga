d359601aec01962a4087b7d3e4d49e2165e32576
wow <mgupta8143@gmail.com>
2022-01-25 00:56:52 -0500

    Test

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,10 @@ import { SessionProvider } from "next-auth/react"
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
-}) {
+}) { 
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Component {...pageProps} /> 
     </SessionProvider> 
   )
 }  
