56b1e1fafebfce08612db03fc0b4476bcc41c765
Teddy Feldmann <teddy.m.feldmann@gmail.com>
2022-02-06 19:51:38 -0500

    Initial commit, made links, set up tailwind
    Added styling to login page, and added navigation functionality to admin page.

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,55 @@
-import {useSession, signIn, signOut} from "next-auth/react"
+import { useSession, signIn, signOut } from 'next-auth/react';
+import Link from 'next/link';
+
+const MainButton = (props) => {
+  return (
+    <Link href={props.href} passHref>
+      <button className="border-blue-900 border-2 hover:bg-blue-900 hover:text-white text-black text-2xl font-bold py-4 px-4 rounded-2xl my-2 mx-4 w-2/4">
+        <a>{props.message}</a>
+      </button>
+    </Link>
+  );
+};
+
+const SignInOrOutButton = (props) => {
+  console.log(props);
+  return (
+    <button
+      onClick={() => {
+        props.signIn();
+      }}
+      className="bg-blue-900 rounded-full py-2 px-4 m-8 text-white hover:bg-white hover:text-blue-900 hover:border-2 hover:border-blue-900"
+    >
+      {props.children}
+    </button>
+  );
+};
 
 export default function Component() {
-  const { data: session } = useSession()
+  const { data: session } = useSession();
+
   if (session) {
     return (
-      <> 
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
+      <div>
+        <div className="flex flex-row items-center justify-end h-16 w-full">
+          <SignInOrOutButton signIn={signOut}>Sign Out</SignInOrOutButton>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <MainButton href="/campaigns" message="Manage Campaigns" />
+          <MainButton href="/volunteers" message="Manage Volunteers" />
+          <MainButton href="/legislators" message="Manage Legislators" />
+          <MainButton href="/counties" message="Manage Counties" />
+        </div>
+      </div>
+    );
   }
   return (
-    <>
-      Not signed in. <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  )
+    <div className="w-full flex flex-col items-center justify-center my-24">
+      <div className="border-2 border-blue-900 p-4">
+        <h1 className="font-bold text-xl">Fair Districts GA</h1>
+        <p>You are not signed in.</p>
+        <SignInOrOutButton signIn={signIn}>Sign In</SignInOrOutButton>
+      </div>
+    </div>
+  );
 }
