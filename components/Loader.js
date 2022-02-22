91420d6e6cf97d1259f189b8eadac6d1b34be97e
Teddy Feldmann <teddy.m.feldmann@gmail.com>
2022-02-22 01:09:51 -0500

    Finished loader component

--- components/Loader.js
+++ components/Loader.js
@@ -1,5 +1,20 @@
+import { Spinner, Center, Stack, Text } from '@chakra-ui/react';
+
 const Loader = () => {
-  return <div>Loading...</div>;
+  return (
+    <Center>
+      <Stack mt="200px" align="center">
+        <Text fontSize="2xl">Loading...</Text>
+        <Spinner
+          size="xl"
+          thickness="4px"
+          speed="0.8s"
+          emptyColor="blue.200"
+          color="blue.800"
+        />
+      </Stack>
+    </Center>
+  );
 };
 
 export default Loader;
