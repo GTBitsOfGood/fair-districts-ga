fe780a3fa7e8db4a8b9c538c60f444c8538006c3
Teddy Feldmann <teddy.m.feldmann@gmail.com>
2022-03-12 10:31:42 -0500

    mid sprint 3

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,109 @@ import Link from 'next/link';
 import React from 'react';
 import NavBar from '../components/NavBar';
 import Loader from '../components/Loader';
+import { Button } from '@chakra-ui/react';
+
+import { Box, Divider, Image, Stack } from '@chakra-ui/react';
+import {
+  MdOutlineCampaign,
+  MdOutlinePeople,
+  MdOutlinePerson,
+} from 'react-icons/md';
+import { FiHome, FiLogIn, FiLogOut } from 'react-icons/fi';
+import { BsNewspaper } from 'react-icons/bs';
+import { VscLaw } from 'react-icons/vsc';
+
+const MainNav = ({ session }) => {
+  return (
+    <Stack padding={2} width={180} direction="column">
+      <Image
+        src="/FairDistrictsGA-Logo.png"
+        objectFit="cover"
+        maxWidth={160}
+        maxHeight={172}
+        paddingBottom={15}
+      />
+      <Divider />
+      <Stack direction="column" spacing={3} paddingTop={2}>
+        <Link href="/">
+          <Button
+            leftIcon={<FiHome />}
+            variant="ghost"
+            justifyContent="flex-start"
+          >
+            Home
+          </Button>
+        </Link>
+        <Link href="/campaign">
+          <Button
+            leftIcon={<MdOutlineCampaign />}
+            variant="ghost"
+            justifyContent="flex-start"
+          >
+            Campaign
+          </Button>
+        </Link>
+        <Link href="/volunteer">
+          <Button
+            leftIcon={<MdOutlinePeople />}
+            variant="ghost"
+            justifyContent="flex-start"
+          >
+            Volunteer
+          </Button>
+        </Link>
+        <Link href="/newspaper">
+          <Button
+            leftIcon={<BsNewspaper />}
+            variant="ghost"
+            justifyContent="flex-start"
+          >
+            Newspapers
+          </Button>
+        </Link>
+        <Link href="/legislator">
+          <Button
+            leftIcon={<VscLaw />}
+            variant="ghost"
+            justifyContent="flex-start"
+          >
+            Legislators
+          </Button>
+        </Link>
+      </Stack>
+      <Box color="white" paddingTop={250} />
+      <Divider />
+      <Stack orientation="vertical" spacing={3}>
+        <Button
+          leftIcon={<MdOutlinePerson />}
+          variant="ghost"
+          justifyContent="flex-start"
+        >
+          Profile
+        </Button>
+        {session ? (
+          <Button
+            leftIcon={<FiLogOut />}
+            variant="ghost"
+            onClick={signOut}
+            justifyContent="flex-start"
+          >
+            Log Out
+          </Button>
+        ) : (
+          <Button
+            leftIcon={<FiLogIn />}
+            variant="ghost"
+            onClick={signIn}
+            justifyContent="flex-start"
+          >
+            Log In
+          </Button>
+        )}
+      </Stack>
+    </Stack>
+  );
+};
 
 const MainButton = (props) => {
   return (
@@ -11,6 +114,21 @@ const MainButton = (props) => {
         <a>{props.message}</a>
       </button>
     </Link>
+  );
+};
+
+const MainButton2 = (props) => {
+  return (
+    <Button
+      size="md"
+      borderRadius="3xl"
+      px="20px"
+      py="16px"
+      color="white"
+      bg="purple.700"
+    >
+      <a>{props.message}</a>
+    </Button>
   );
 };
 
@@ -27,12 +145,6 @@ const SignInOrOutButton = (props) => {
   );
 };
 
-/*
-TODO:
-change navbar to go all the way across, remove admin menu
-design loading spinner -- something custom or with Chakra
-remove tailwindcss
-*/
 const MainPageMenu = ({ session }) => {
   return (
     <div className="flex flex-row">
@@ -40,12 +152,14 @@ const MainPageMenu = ({ session }) => {
       <div className="flex flex-col w-full">
         <div className="flex flex-row items-center justify-end h-16 w-full">
           <SignInOrOutButton signIn={signOut}>Sign Out</SignInOrOutButton>
+          <MainButton2 message="Sign Out" />
         </div>
         <div className="flex flex-col items-center justify-center">
           <MainButton href="/campaigns" message="Manage Campaigns" />
           <MainButton href="/volunteers" message="Manage Volunteers" />
           <MainButton href="/legislators" message="Manage Legislators" />
           <MainButton href="/counties" message="Manage Counties" />
+          <MainButton2 message="Sign Out" />
         </div>
       </div>
     </div>
@@ -57,25 +171,28 @@ export default function Component() {
 
   const OtherComponent = React.lazy(() => MainPageMenu);
 
-  // function MyComponent() {
-  //   return (
-  //     // Displays <Spinner> until OtherComponent loads
-  //     <React.Suspense fallback={<Spinner />}>
-  //       <div>
-  //         <OtherComponent />
-  //       </div>
-  //     </React.Suspense>
-  //   );
-  // }
   if (session === undefined) {
     return <Loader />;
   }
   if (session) {
     return (
       <React.Suspense fallback={<Loader />}>
-        <div>
-          <MainPageMenu session={session} />
+        <div className="flex flex-row">
+          <MainNav session={session} />
+          <div className="flex flex-col w-full">
+            <div className="flex flex-row items-center justify-end h-16 w-full"></div>
+            <div className="flex flex-col items-center justify-center">
+              <MainButton href="/campaigns" message="Manage Campaigns" />
+              <MainButton href="/volunteers" message="Manage Volunteers" />
+              <MainButton href="/legislators" message="Manage Legislators" />
+              <MainButton href="/counties" message="Manage Counties" />
+            </div>
+          </div>
         </div>
+        {/* <div>
+          <MainPageMenu session={session} />
+          <MainNav session={session} />
+        </div> */}
       </React.Suspense>
     );
   }
