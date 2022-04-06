aa3d4b75c6b51e812be88b69cefc0e4314f30415
Katsuki Chan <katsukidotxyz@gmail.com>
2022-04-05 21:02:27 -0400

    fix padding

--- components/Campaign/Footer.js
+++ components/Campaign/Footer.js
@@ -2,17 +2,11 @@ import React from "react";
 import { Box, Divider, Flex, Button } from "@chakra-ui/react";
 import { If, Else, Then } from "react-if";
 
-const CampaignFooter = ({
-  hasBack = true,
-  hasNext = true,
-  //   currentPage,
-  //   setCurrentPage,
-  decrementPage,
-}) => {
+const CampaignFooter = ({ hasBack = true, hasNext = true, decrementPage }) => {
   return (
     <Box mt={6}>
       <Divider color="gray.400" mb={4} />
-      <Flex justifyContent="space-between">
+      <Flex justifyContent="space-between" pb={2}>
         <If condition={hasBack}>
           <Then>
             <Button colorScheme="gray" onClick={decrementPage}>
